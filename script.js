cb60917daf665389187030cb95c9d909564d7d03
isabellacortesoliveira-byte <isabella.cortes.oliveira@escola.pr.gov.br>
2026-08-25 15:24:35 +0000

    oii

---
+++ script.js
@@ -0,0 +1,8 @@
+function mostrarMensagem() {
+
+    const mensagem = document.getElementById("oii");
+
+    mensagem.textContent =
+        "Nossa solução foi criada para ser simples, acessível e ajudar o usuário a organizar melhor sua rotina.";
+
+}
